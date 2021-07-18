a846d1e8c35c57430ad27dddae5005b102fff5e2
jaylee <ljay189@gmail.com>
2021-07-18 18:26:58 +0900

    Simple initial commit

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import argparse
+import torch.nn as nn
+
+class CornerNet:
+    def __init__(self):
+        pass
+
+    def get_args(self) -> argparse.Namespace:
+        pass
+
+    def train(self, no_of_epochs: int):
+        pass
+
+    def evaluate(self, pretrained_model: nn.Module,):
+        pass
+
+
+def train(model):
+    """
+    Train the model
+    Args:
+        model: The model to train
+    """
+    pass
+
+
+if __name__ == "__main__":
+    pass
